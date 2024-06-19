da82345b313f2df62eb8656479c97731ffe4719a
Lucas Barbosa da Silva <145168605+Lucasbarbosa332@users.noreply.github.com>
2024-06-19 00:06:39 -0300

    Add files via upload

---
+++ Dageoon puzzy/Dageon pazzy/dist/script.js
@@ -0,0 +1,15 @@
+"use strict";
+////////////////////////////////////////////////////////////////////////////////
+// 
+//
+// S -> The starting point.
+// F -> Escape door.
+// K -> Key to open the locked door.
+// L -> Locked door. How to input: L_x-y where x is the x index (of array)
+//      of K, and y is the y index. Array is declared as 2 dimensional array.
+// W -> Wall. You can't pass here.
+// T -> Tree. You can't pass here.
+// B -> Barrel. You can't pass here.
+// M -> Monster. Don't go here, you'll lose.
+// <space> -> Floor where you can walk.
+////////////////////////////////////////////////////////////////////////////////
